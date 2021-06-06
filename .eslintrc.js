2f3f9fe85c745f0df3f30bb50529f6f551edb3c6
lvjiahong <1473074667@qq.com>
2021-06-07 00:13:43 +0800

    格式化处理code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  "extends": ["taro/react"],
+  "parser": "@typescript-eslint/parser",
+  "extends": ["taro/react","@typescript-eslint/eslint-plugin"],
   "rules": {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off"
+  },
+  "extends": [
+    "alloy",
+    "alloy/react",
+    "alloy/typescript"
+  ],
+  "settings": {
+    "react": {
+        "version": "detect"
+    }
   }
 }
