12640c8d6617a372741eda7791a72e629267cdc7
lvjiahong <1473074667@qq.com>
2021-06-15 18:11:53 +0800

    框架基础处理233

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,12 @@
-import { Component } from 'react';
-import { View, Text } from '@tarojs/components';
-import { AtButton, AtRate } from 'taro-ui';
+import { Component } from "react";
+import { View, Text } from "@tarojs/components";
+import { AtButton, AtRate } from "taro-ui";
 
-import 'taro-ui/dist/style/components/button.scss'; // 按需引入
-import './index.scss';
+import "taro-ui/dist/style/components/button.scss"; // 按需引入
+import "./index.scss";
 
 export default class Index extends Component {
-  /**  */
+  /** 构造器 */
   public constructor() {
     super(...arguments);
     this.state = {
@@ -17,7 +17,7 @@ export default class Index extends Component {
   /** 处理函数 */
   public handleChange(value) {
     this.setState({
-      value
+      state: value
     });
   }
 
@@ -39,15 +39,15 @@ export default class Index extends Component {
   /**  */
   public render() {
     return (
-      <View className="index">
+      <View className='pages'>
         <Text>Hello world!</Text>
-        <AtButton type="primary">I need Taro UI</AtButton>
+        <AtButton type='primary'>I need Taro UI</AtButton>
         <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type="primary" circle={true}>
+        <AtButton type='primary' circle={true}>
           支持
         </AtButton>
         <Text>共建？</Text>
-        <AtButton type="secondary" circle={true}>
+        <AtButton type='secondary' circle={true}>
           来
         </AtButton>
         <AtRate value={this.state.value} onChange={this.handleChange} />
